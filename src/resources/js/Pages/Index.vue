fc6c8b336f7720b1eb00583b03708c62cff3f8c4
ugr0 <yujiro.takahashi0912@gmail.com>
2023-07-04 15:17:51 +0900

    style: apply tailwind class

--- src/resources/js/Pages/Index.vue
+++ src/resources/js/Pages/Index.vue
@@ -1,27 +1,44 @@
 <script setup>
 import KinstaLayout from "../Layouts/KinstaLayout.vue";
 
-defineProps({
-  articles: Object,
-});
+  defineProps({
+    articles: Object,
+  });
 </script>
 
 <template>
-    <KinstaLayout>
-      <h2>Read our latest articles</h2>
+ <KinstaLayout>
+    <h2 class="text-2xl font-bold py-10">Read our latest articles</h2>
 
-      <section>
-        // Looping over articles
-        <article v-for="article in articles" :key="article.id">
-          <div>
-            <img src="/images/kinsta-logo.png" alt="Article thumbnail" />
-          </div>
+    <section class="space-y-5 border-b-2 pb-10">
+      <article
+        v-for="article in articles"
+         :key="article.id"
+        class="flex justify-center items-center shadow-md bg-white rounded-xl p-4 mx-auto max-w-3xl"
+      >
 
-          <h3>{{article.title}}</h3>
-          <p>{{article.excerpt}}</p>
+         <img
+            src="/images/kinsta-logo.png"
+            class="w-32 h-32 rounded-xl object-cover"
+            alt=""
+         />
 
-          <a href="#">Read more</a>
-        </article>
-      </section>
-    </KinstaLayout>
+        <div class="flex flex-col text-left justify-between pl-3 space-y-5">
+          <h3
+            class="text-xl font-semibold text-indigo-600 hover:text-indigo-800"
+          >
+            <a href="#">{{ article.title }}</a>
+          </h3>
+          <p>
+           {{ article.excerpt }}
+          </p>
+          <a
+            href="#"
+            class="text-indigo-600 hover:text-indigo-800 w-fit self-end font-semibold"
+            >Read more</a
+          >
+        </div>
+      </article>
+    </section>
+ </KinstaLayout>
 </template>
