d531c47bc9221ae387306af83f2192d4532426c2
ugr0 <yujiro.takahashi0912@gmail.com>
2023-07-04 12:57:23 +0900

    feat: create vue pages

---
+++ src/resources/js/Pages/Show.vue
@@ -0,0 +1,23 @@
+<script setup>
+  //
+</script>
+
+<template>
+  <header>
+    <h1>Welcome to Kinsta Blog</h1>
+  </header>
+
+  <main>
+    <article>
+      <h1>Title for the blog</h1>
+
+      <p>Article content goes here</p>
+    </article>
+  </main>
+
+  <footer>
+    <h2>Join our Newsletter</h2>
+
+    <input type="email" />
+  </footer>
+</template>
